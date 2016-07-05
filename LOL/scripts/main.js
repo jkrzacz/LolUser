cd77868a3c15c1f24152860e295fd9fa24406017
Janusz <Janusz@JANUSZ-KOMPUTER>
2016-07-06 00:13:21 +0200

    Beta Version

--- LOL/scripts/main.js
+++ LOL/scripts/main.js
@@ -1,4 +1,5 @@
 ﻿$(function () {
+    
     $("#searchButton").click(function () {
         var apiKeyParameters = "?api_key=024a9118-11db-4339-af39-1b9e1db3420c";
         var region = $("#region").val();
@@ -23,8 +24,9 @@
         })
     });
 
-
+    
     function getServerStatus(userId) {
+        $("#ServerInfo").empty();
         var region = $("#region").val();
         var url = "http://status.leagueoflegends.com/shards/" + region;
         $.ajax({
@@ -48,6 +50,7 @@
         })
     }
     function getPlayerLeague(userId, name) {
+        $("#PlayerStats").empty();
         var region = $("#region").val();
         var url = "https://" + region + ".api.pvp.net/api/lol/" + region + "/v2.5/league/by-summoner/" + userId + "/entry?api_key=024a9118-11db-4339-af39-1b9e1db3420c";
         $.ajax({
@@ -65,6 +68,7 @@
     }
 
     function getstatss2016(userId) {
+        $("#SeasonStats2016").empty();
         var region = $("#region").val();
         var url = "https://" + region + ".api.pvp.net/api/lol/" + region + "/v1.3/stats/by-summoner/" + userId + "/summary?season=SEASON2016&api_key=024a9118-11db-4339-af39-1b9e1db3420c";
         $.ajax({
@@ -133,6 +137,7 @@
 
     }
     function getstatss2015(userId) {
+        $("#SeasonStats2015").empty();
         var region = $("#region").val();
         var url = "https://" + region + ".api.pvp.net/api/lol/" + region + "/v1.3/stats/by-summoner/" + userId + "/summary?season=SEASON2015&api_key=024a9118-11db-4339-af39-1b9e1db3420c";
         $.ajax({
@@ -201,6 +206,7 @@
 
     }
     function getstatss2014(userId) {
+        $("#SeasonStats2014").empty();
         var region = $("#region").val();
         var url = "https://" + region + ".api.pvp.net/api/lol/" + region + "/v1.3/stats/by-summoner/" + userId + "/summary?season=SEASON2014&api_key=024a9118-11db-4339-af39-1b9e1db3420c";
         $.ajax({
@@ -268,6 +274,7 @@
 
     }
     function getstatss2013(userId) {
+        $("#SeasonStats2013").empty();
         var region = $("#region").val();
         var url = "https://" + region + ".api.pvp.net/api/lol/" + region + "/v1.3/stats/by-summoner/" + userId + "/summary?season=SEASON3&api_key=024a9118-11db-4339-af39-1b9e1db3420c";
         $.ajax({
@@ -341,6 +348,66 @@
         return myImage;
 
     }
+    function getItemImage(itemId, width, height) {
+        var url = "http://ddragon.leagueoflegends.com/cdn/6.13.1/img/item/" + itemId + ".png";
+        var myImage = new Image(width, height);
+        myImage.src = url;
+
+        return myImage;
+
+    }
+    function getSpellImage(SpellId, width, height) {
+        var url = "";
+        var myImage = new Image(width, height);
+        switch (SpellId) {
+            case 1:
+                url = "http://vignette2.wikia.nocookie.net/leagueoflegends/images/9/95/Cleanse.png/revision/latest?cb=20140115125907";
+                break;
+            case 12:
+                url = "http://vignette3.wikia.nocookie.net/leagueoflegends/images/e/e8/Teleport.png/revision/latest?cb=20140115125728";
+                break;
+            case 30:
+                url = "http://vignette2.wikia.nocookie.net/leagueoflegends/images/9/9d/To_the_King%21.png/revision/latest?cb=20141125095423";
+                break;
+            case 14:
+                url = "http://vignette3.wikia.nocookie.net/leagueoflegends/images/f/f4/Ignite.png/revision/latest?cb=20131212215221";
+                break;
+            case 6:
+                url = "http://vignette2.wikia.nocookie.net/leagueoflegends/images/a/ab/Ghost.png/revision/latest?cb=20140115125935";
+                break;
+            case 32:
+                url = "http://vignette2.wikia.nocookie.net/leagueoflegends/images/4/4d/Mark.png/revision/latest?cb=20150410192947";
+                break;
+            case 7:
+                url = "http://vignette2.wikia.nocookie.net/leagueoflegends/images/6/6e/Heal.png/revision/latest?cb=20111115121115";
+                break;
+            case 11:
+                url = "http://vignette3.wikia.nocookie.net/leagueoflegends/images/0/05/Smite.png/revision/latest?cb=20140115125806";
+                break;
+            case 31:
+                url = "http://vignette1.wikia.nocookie.net/leagueoflegends/images/8/83/Poro_Toss.png/revision/latest/scale-to-width-down/36?cb=20141125095041";
+                break;
+            case 3:
+                url = "http://vignette2.wikia.nocookie.net/leagueoflegends/images/4/4a/Exhaust.png/revision/latest?cb=20111115121116";
+                break;
+            case 13:
+                url = "http://vignette2.wikia.nocookie.net/leagueoflegends/images/d/d7/Clarity.png/revision/latest?cb=20131212215533";
+                break;
+            case 2:
+                url = "http://vignette4.wikia.nocookie.net/leagueoflegends/images/2/21/Clairvoyance.png/revision/latest?cb=20140115125838";
+                break;
+            case 21:
+                url = "http://vignette2.wikia.nocookie.net/leagueoflegends/images/c/cc/Barrier.png/revision/latest?cb=20140115125640";
+                break;
+            case 4:
+                url = "http://vignette1.wikia.nocookie.net/leagueoflegends/images/7/74/Flash.png/revision/latest?cb=20140115125446";
+                break;
+        }
+        myImage.src = url;
+
+        return myImage;
+
+    }
     function championStats(userId) {
         var region = $("#region").val();
         var url = "https://global.api.pvp.net/api/lol/static-data/" + region + "/v1.2/champion?champData=all&api_key=024a9118-11db-4339-af39-1b9e1db3420c";
@@ -375,7 +442,9 @@
     function showRecentGames(userId, champId, RGames) {
         for (var i = 0; i < 10; i++) {
             var a = "#Game" + i;
-            $( a + " #Image").html(getChampImage(champId[RGames[i].championId], 100, 100));
+            $(a + " #Image").empty();
+            $(a + " #Image").html(getChampImage(champId[RGames[i].championId], 100, 100)).append("<div>" + champId[RGames[i].championId] + "</div>");
+
             if (RGames[i].stats.win) {
                 $(a + " #Status").html("<td style=\"font-size: 18px; font-weight: bold; color: #99FF44;\">Win</td>")
                 $(a).css("background", "linear-gradient(rgba(75, 255, 55, 0.5), rgba(75, 255, 55, 0.3))");
@@ -386,9 +455,33 @@
             $(a + " #Name").html("<td>" + RGames[i].subType + "</td>");
             $(a + " #Stats").html("<td>" + RGames[i].stats.championsKilled + "/" + RGames[i].stats.assists + "/" + RGames[i].stats.numDeaths + "</td>");
             var KDA = (RGames[i].stats.championsKilled + RGames[i].stats.assists) / RGames[i].stats.numDeaths;
-            $(a + " tbody").append("<tr><td>" + KDA + " KDA</td></tr>");
-            $(a + " tbody").append("<tr><td>" + RGames[i].stats.goldEarned + " Gold</td></tr>");
-            $(a + " tbody").append("<tr><td>" + RGames[i].stats.minionsKilled + " Creeps</td></tr>");
+            $(a + " #KDA").html("<td>" + KDA + " KDA</td>");
+            $(a + " #Gold").html("<td>" + RGames[i].stats.goldEarned + " Gold</td>");
+            $(a + " #Creeps").html("<td>" + RGames[i].stats.minionsKilled + " Creeps</td>");
+            $(a + " #Spell1").empty();
+            $(a + " #Spell2").empty();
+            $(a + " #Spell1").append("<td>").append(getSpellImage(RGames[i].spell1, 50, 50)).append("</td>");
+            $(a + " #Spell2").append("<td>").append(getSpellImage(RGames[i].spell2, 50, 50)).append("</td>");
+            $(a + " #ItemText").empty();
+            $(a + " #Items").empty();
+            $(a + " #ItemText").html("<strong>Items</strong>");
+            $(a + " #Items").append("<div>");
+            if (RGames[i].stats.item0) { $(a + " #Items").append(getItemImage(RGames[i].stats.item0, 40, 40)); }
+            else { $(a + " #Items").append(getItemImage(3637, 40, 40)); }
+            if (RGames[i].stats.item1) { $(a + " #Items").append(getItemImage(RGames[i].stats.item1, 40, 40)); }
+            else { $(a + " #Items").append(getItemImage(3637, 40, 40)); }
+            if (RGames[i].stats.item2) { $(a + " #Items").append(getItemImage(RGames[i].stats.item2, 40, 40)); }
+            else { $(a + " #Items").append(getItemImage(3637, 40, 40)); }
+            if (RGames[i].stats.item6) { $(a + " #Items").append(getItemImage(RGames[i].stats.item6, 40, 40)); }
+            else { $(a + " #Items").append(getItemImage(3637, 40, 40)); }
+            $(a + " #Items").append("</div><div>");
+            if (RGames[i].stats.item3) { $(a + " #Items").append(getItemImage(RGames[i].stats.item3, 40, 40)); }
+            else { $(a + " #Items").append(getItemImage(3637, 40, 40)); }
+            if (RGames[i].stats.item4) { $(a + " #Items").append(getItemImage(RGames[i].stats.item4, 40, 40)); }
+            else { $(a + " #Items").append(getItemImage(3637, 40, 40)); }
+            if (RGames[i].stats.item5) { $(a + " #Items").append(getItemImage(RGames[i].stats.item5, 40, 40)); }
+            else { $(a + " #Items").append(getItemImage(3637, 40, 40)); }
+            $(a + " #Items").append("</div>");
         }
         
         
